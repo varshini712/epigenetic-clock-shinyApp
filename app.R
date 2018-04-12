0383a68dee987ab96c5a30fdb1ecd05c9936cdeb
Varshini Vasudevaraja <varshini712@users.noreply.github.com>
2018-04-12 12:57:09 -0400

    updated app.R

--- app.R
+++ app.R
@@ -1,11 +1,11 @@
 library(shiny)
 library(sqldf)
 library(readr)
-
+library(DT)
 
 ui <- fluidPage(theme = "main.css",
   navbarPage("EpGClock",
-    tabPanel("Home",fluidRow(column(6,style="background-image:url(\"main.jpeg\");",div(style="height:870px;")),
+    tabPanel("Home",fluidRow(column(6,style="background-image:url(\"main.jpeg\");background-repeat:no-repeat;",div(style="height:1500px;")),
                              column(6,h1("Epigenetic Clock"),br(),br(),
       p("Epigenetic clock, also called as DNAmAge is used to predict the DNA methylation age of tissues and cell types using data generated from Illumina Infinium platform.
         This application is developed based on the code and instructions provided by Steve Horvath who developed the methylation age calculator."),
@@ -26,9 +26,6 @@ ui <- fluidPage(theme = "main.css",
                   multiple = FALSE
                           ),
               hr(),
-              helpText("Enter a sample id (for example hESCs_1) and press the button"),
-              textInput(inputId = "sampleID", label = "Enter a sampleID/sampleName", placeholder = ""),
-              actionButton(inputId = "calculate", label = "Predict age"),
               hr(),
               p("Based on",
               a(href="https://doi.org/10.1186/gb-2013-14-10-r115", "\"DNA methylation age of human tissues and cell types\""),
@@ -37,15 +34,27 @@ ui <- fluidPage(theme = "main.css",
             )
       ),
     
-            mainPanel(tableOutput("table")))),
+            mainPanel(DT::dataTableOutput("table")))),
 
-    tabPanel("Correlation with Chronological Age")
+    tabPanel("Correlation with Chronological Age",
+             sidebarLayout(
+               sidebarPanel(
+                 helpText("Upload a .csv file of SampleAnnotation that contains the chronological ages for which the DNAm Age was ")
+               ),
+               mainPanel()
+             ))
   )
 )
 server <- function(input,output){
   options(shiny.maxRequestSize = 500 * 1024 ^ 2)
   source("horvath.R")
-  output$table <- renderTable({
+  
+  output$table <- renderDataTable({
+    withProgress(message = 'Calculation in progress', detail = 'This may take a while...',value = 0, {
+      for(i in 1:15){
+        incProgress(1/20)
+        Sys.sleep(0.25)
+      }
     req(input$betaFile)
     dat0 = read.csv.sql(input$betaFile$datapath,header = TRUE,sep = ",")
     datMiniAnnotation=read.csv("datMiniAnnotation.csv")
@@ -62,9 +71,7 @@ server <- function(input,output){
     
     ##Calculate DNAm age tabsetpanel##
     final <- calculateAge(datout)
+    })
   })
 }
 shinyApp(ui = ui, server = server)
-
-#fluidRow(HTML('<img src = "main1.png" class="responsive"></img>')),
-#fluidRow(HTML('<p>DNAm Age Calculator</p>'))
